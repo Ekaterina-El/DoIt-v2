e5893faea2ac32e08561c34327470b338c1e6237
Ekaterina-El <hackerrussia@yandex.ru>
2022-03-01 12:22:22 +0500

    Added EditTodoScreen

---
+++ app/src/main/java/el/ka/doit_v2/screens/editTodoScreen/EditTodoFragment.kt
@@ -0,0 +1,6 @@
+package el.ka.doit_v2.screens.editTodoScreen
+
+import androidx.fragment.app.Fragment
+import el.ka.doit_v2.R
+
+class EditTodoFragment : Fragment(R.layout.fragment_edit_todo)
---
+++ app/src/main/java/el/ka/doit_v2/screens/editTodoScreen/EditTodoViewModel.kt
@@ -0,0 +1,6 @@
+package el.ka.doit_v2.screens.editTodoScreen
+
+import androidx.lifecycle.ViewModel
+
+class EditTodoViewModel: ViewModel() {
+}
