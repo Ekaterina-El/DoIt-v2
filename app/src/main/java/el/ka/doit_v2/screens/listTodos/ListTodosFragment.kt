2697368cfe8e8441473ccfc856459fd0ea97e14a
Ekaterina-El <hackerrussia@yandex.ru>
2022-03-01 11:58:23 +0500

    Created list todo layout

---
+++ app/src/main/java/el/ka/doit_v2/screens/listTodos/ListTodosFragment.kt
@@ -0,0 +1,6 @@
+package el.ka.doit_v2.screens.listTodos
+
+import androidx.fragment.app.Fragment
+import el.ka.doit_v2.R
+
+class ListTodosFragment : Fragment(R.layout.fragment_list_todos)
---
+++ app/src/main/java/el/ka/doit_v2/screens/listTodos/ListTodosViewModel.kt
@@ -0,0 +1,6 @@
+package el.ka.doit_v2.screens.listTodos
+
+import androidx.lifecycle.ViewModel
+
+class ListTodosViewModel: ViewModel() {
+}
