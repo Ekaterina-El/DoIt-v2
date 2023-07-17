99949998b110ae01f4463ba84ef745c8a36f517e
EkaterinaEl <hackerrussia@yandex.ru>
2023-07-17 13:24:29 +0500

    added deps for safe args

--- app/src/main/java/el/ka/doit_v2/presentation/MainActivity.kt
+++ app/src/main/java/el/ka/doit_v2/presentation/MainActivity.kt
@@ -2,8 +2,6 @@ package el.ka.doit_v2.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import el.ka.doit_v2.R
 
 class MainActivity : AppCompatActivity() {
